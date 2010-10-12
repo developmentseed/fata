81a54b105ae2c50664b2488e9d91c6873ad341c9
Will White <will@developmentseed.org>
2010-10-12 14:48:55 -0400

    Fix indentation in app.js.

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var connect = require('connect'),
     settings = require('settings');
 
 if (settings.db && settings.db.url) {
-  var mongoose = require('mongoose/mongoose').Mongoose;
-  var db = mongoose.connect(settings.db.url);
-  console.log('Using database %s', settings.db.url)
+    var mongoose = require('mongoose/mongoose').Mongoose;
+    var db = mongoose.connect(settings.db.url);
+    console.log('Using database %s', settings.db.url)
 }
 
 // Initialize core object.
 var app = module.exports = new express.Server([
-  connect.logger(),
-  connect.staticProvider(__dirname + '/public')
+    connect.logger(),
+    connect.staticProvider(__dirname + '/public')
 ]);
 
 app.listen(settings.port);
@@ -21,19 +21,19 @@ console.log('Express server started on port %s', app.address().port);
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
-  res.render('index', {
-      locals: { pageTitle: 'Home | ' + settings.siteTitle }
-  });
+    res.render('index', {
+        locals: { pageTitle: 'Home | ' + settings.siteTitle }
+    });
 });
 
 app.get('/agency/:id?', function(req, res) {
-  res.render('agency', {
-      locals: { pageTitle: 'Agency | ' + settings.siteTitle }
-  });
+    res.render('agency', {
+        locals: { pageTitle: 'Agency | ' + settings.siteTitle }
+    });
 });
 
 app.get('/question/:id?', function(req, res) {
-  res.render('question', {
-      locals: { pageTitle: 'Qeustion | ' + settings.siteTitle }
-  });
+    res.render('question', {
+        locals: { pageTitle: 'Qeustion | ' + settings.siteTitle }
+    });
 });
