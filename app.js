836631dd17701cebbb2492afbfad1cfb2f3a7921
Will White <will@developmentseed.org>
2010-10-12 15:38:57 -0400

    Add dynamicHelpers.

--- app.js
+++ app.js
@@ -19,21 +19,39 @@ var app = module.exports = new express.Server([
 app.listen(settings.port);
 console.log('Express server started on port %s', app.address().port);
 app.set('view engine', 'hbs');
+app.dynamicHelpers({
+    siteTitle: function(req, res) {
+        return settings.siteTitle;
+    },
+    primaryNavigation: function(req) {
+        items = [
+            {title:'Home', url:'/'},
+            {title:'Agencies', url:'/agency'},
+            {title:'Questions', url:'/question'}
+        ];
+        for (item in items) {
+            if (items[item].url == req.url) {
+                items[item].active = true;
+            }
+        }
+        return items;
+    },
+});
 
 app.get('/', function(req, res) {
     res.render('index', {
-        locals: { pageTitle: 'Home | ' + settings.siteTitle }
+        locals: { pageTitle: 'Home' }
     });
 });
 
 app.get('/agency/:id?', function(req, res) {
     res.render('agency', {
-        locals: { pageTitle: 'Agency | ' + settings.siteTitle }
+        locals: { pageTitle: 'Agency' }
     });
 });
 
 app.get('/question/:id?', function(req, res) {
     res.render('question', {
-        locals: { pageTitle: 'Qeustion | ' + settings.siteTitle }
+        locals: { pageTitle: 'Question' }
     });
 });
