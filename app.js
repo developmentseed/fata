9f16c4a04beedcef0e42e563d476a7c5cf5acd27
Will White <will@developmentseed.org>
2010-10-13 22:57:08 -0400

    Added query to question page.

--- app.js
+++ app.js
@@ -174,27 +174,86 @@ app.get('/agency/:id/:filter?', function(req, res, next) {
 });
 
 // Handle question page
-app.get('/question/:id/:filter?', function(req, res) {
-    if (settings.questions.indexOf(req.params.id) !== -1) {
-        var question = req.params.id,
-            dataHandler = req.dataHandler;
-        dataHandler.countField('responses', question, {}, function(result) {
-            data = [];
-            _.each(result[question], function(value, key) {
-                response = {
-                    name: key,
-                    count: value
-                };
-                data.push(response);
-            });
-            res.render('question', {
-                locals: {
-                    pageTitle: 'Question',
-                    results: data
-                }
-            });
+app.get('/question/:group/:filter?', function(req, res) {
+    var group = req.params.group,
+        dataHandler = req.dataHandler,
+        async = require('async'),
+        waterfall = [];
+    // Load the question group
+    waterfall.push(function (callback) {
+        dataHandler.field('questions', {id:group}, function(groups) {
+            callback(null, groups[0]);
         });
-    }
+    });
+    // Load list of agencies
+    waterfall.push(function(group, callback) {
+        dataHandler.field('agencies', {}, function(agencies) {
+            callback(null, group, agencies);
+        });
+    });
+    // Get response counts per question, per agency.
+    waterfall.push(function (group, agencies, callback) {
+        var parallel = [],
+            responses = {};
+        _.each(group.questions, function(value, question) {
+            if (value.display.indexOf('question') !== -1) {
+                responses[question] = {text:value.name};
+                agencies.forEach(function(agency) {
+                    responses[question][agency.ID] = [];
+                    parallel.push(function(callback) {
+                        dataHandler.countField('responses', question, {Agency:agency.ID}, function(results) {
+                            response = {};
+                            // Add a total response count
+                            response.total = _.reduce(results, function(memo, num){
+                                return memo + num.value.count;
+                            }, 0);
+                            // Add raw count and percentage
+                            _.each(results, function(num, key) {
+                                _.each(results, function(num){
+                                    response[num._id] = {
+                                        count: num.value.count,
+                                        percent: num.value.count / response.total * 100
+                                    }
+                                });
+                            });
+                            responses[question][agency.ID].push(response);
+                            callback(null);
+                        });
+                    });
+                });
+            }
+        });
+        async.parallel(parallel, function() {
+            callback(null, responses);
+        });
+    });
+    // Render the page
+    waterfall.push(function(results, callback) {
+        console.log(results);
+        res.render('question', {
+            locals: {
+                pageTitle: 'Question',
+                questions: results
+            }
+        });
+    });
+    // dataHandler.countField('responses', question, {}, function(result) {
+    //     data = [];
+    //     _.each(result[question], function(value, key) {
+    //         response = {
+    //             name: key,
+    //             count: value
+    //         };
+    //         data.push(response);
+    //     });
+    //     res.render('question', {
+    //         locals: {
+    //             pageTitle: 'Question',
+    //             results: data
+    //         }
+    //     });
+    // });
+    async.waterfall(waterfall);
 });
 
 // Handle about page
