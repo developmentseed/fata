5a426c370c805b2b063caa44dda388cd2885cbc8
Tom MacWright <macwright@gmail.com>
2010-10-14 18:21:10 -0400

    Updating styles, adding drone style

--- page.map.js
+++ page.map.js
@@ -7,17 +7,22 @@ app.get('/map/home', function(req, res) {
     var fs = require('fs');
     var map_template = JSON.parse(fs.readFileSync('map_defaults.json', 'utf-8'));
     var base_layer = map_template.layers.mapbox;
+    var stylewriter_layer = map_template.layers.stylewriter;
     var blockswitcher = map_template.externals.blockswitcher;
 
     base_layer._value[1].layername = 'pakistan-grey';
     base_layer._value[0] = 'FATA';
     base_layer._value[1].type = 'jpg';
 
+    stylewriter_layer._value[0] = 'Attacks';
+    stylewriter_layer._value[1] = 'http://localhost:8887/tile/';
+    stylewriter_layer._value[2].mapfile = 'http://localhost:8888/style/drone/mohmand';
+
     blockswitcher._value[0] = '#home-map';
 
     res.send({
         'map': {
-            'layers': [base_layer],
+            'layers': [base_layer, stylewriter_layer],
             'maxExtent': map_template.maxExtent,
             'maxResolution': 1.40625,
             'projection': map_template.spherical_mercator,
@@ -35,23 +40,29 @@ app.get('/map/agency/:id', function(req, res) {
     var fs = require('fs');
     var map_template = JSON.parse(fs.readFileSync('map_defaults.json', 'utf-8'));
     var base_layer = map_template.layers.mapbox;
+    var stylewriter_layer = map_template.layers.stylewriter;
     var blockswitcher = map_template.externals.blockswitcher;
 
     base_layer._value[1].layername = 'pakistan-grey';
     base_layer._value[0] = 'FATA';
     base_layer._value[1].type = 'jpg';
+    
+    stylewriter_layer._value[0] = 'Attacks';
+    stylewriter_layer._value[1] = 'http://localhost:8887/tile/${mapfile}/${z}/${x}/${y}.${format}';
+    stylewriter_layer._value[2].mapfile = 'http://localhost:8888/style/drone/mohmand';
+    stylewriter_layer._value[2].isBaseLayer = false;
 
     blockswitcher._value[0] = '#agency-map';
 
     res.send({
         'map': {
-            'layers': [base_layer],
+            'layers': [base_layer, stylewriter_layer],
             'maxExtent': map_template.maxExtent,
             'maxResolution': 1.40625,
             'projection': map_template.spherical_mercator,
             'displayProjection': map_template.spherical_mercator,
             'units': 'm',
-            'controls': [map_template.controls.navigation]
+            // 'controls': [map_template.controls.navigation]
         },
         'externals': {
             'layerswitcher': blockswitcher
