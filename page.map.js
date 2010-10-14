3a44483ab5f4e84fece61a3276dddb2d8562baf8
Tom MacWright <macwright@gmail.com>
2010-10-14 12:34:22 -0400

    Homepage maps

--- page.map.js
+++ page.map.js
@@ -13,6 +13,34 @@ app.get('/map/question/:question', function(req, res) {
         'controls': [map_template.controls.navigation]});
 });
 
+app.get('/map/home', function(req, res) {
+    var fs = require('fs');
+    var map_template = JSON.parse(fs.readFileSync('map_defaults.json', 'utf-8'));
+    var base_layer = map_template.layers.mapbox;
+    var blockswitcher = map_template.externals.blockswitcher;
+
+    base_layer._value[1].layername = 'afghanistan-summer-jpg';
+    base_layer._value[0] = 'Afghanistan Summer';
+    base_layer._value[1].type = 'jpg';
+
+    blockswitcher._value[0] = '#home-map';
+
+    res.send({
+        'map': {
+            'layers': [base_layer],
+            'maxExtent': map_template.maxExtent,
+            'maxResolution': 1.40625,
+            'projection': map_template.spherical_mercator,
+            'displayProjection': map_template.spherical_mercator,
+            'units': 'm',
+            'controls': [map_template.controls.navigation]
+        },
+        'externals': {
+            'layerswitcher': map_template.externals.blockswitcher
+        }
+    });
+});
+
 app.get('/map/agency/:id', function(req, res) {
     var fs = require('fs');
     var map_template = JSON.parse(fs.readFileSync('map_defaults.json', 'utf-8'));
