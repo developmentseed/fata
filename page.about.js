429d0db6c1eae4cf947b9c2e443f8d69ddf5239a
Young Hahn <young@developmentseed.org>
2010-10-14 04:17:06 -0400

    About page, generalize .prose class.

--- page.about.js
+++ page.about.js
@@ -5,8 +5,7 @@ var app = module.parent.exports;
 
 app.get('/about', function(req, res) {
     var dataHandler = req.dataHandler;
-    dataHandler.markdown({path: 'content/about.md'}, function(err, data) {
-        if (err) throw err;
+    dataHandler.markdown({path: 'content/about.md'}, function(data) {
         res.render('about', {
             locals: {
                 pageTitle: 'About',
