05c5c49e284d204c279e6ae268c98aef26e5b51c
Young Hahn <young@developmentseed.org>
2011-09-14 13:50:42 -0400

    Finish maps.

---
+++ fata-q11b/style.mss
@@ -0,0 +1,7 @@
+#fata[positive > 0] {
+  polygon-opacity:0.5;
+  [positive_pct < 100] { polygon-fill: #96de6e; }
+  [positive_pct < 30] { polygon-fill: #7ab658; }
+  [positive_pct < 20] { polygon-fill: #6ea44f; }
+  [positive_pct < 10] { polygon-fill: #4b7335; }
+}
---
+++ fata-q11d/data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+question=Q11d
+
+rm layers/$question.sqlite
+curl http://developmentseed.org/fata/csv/$question.csv > layers/$question.csv && \
+echo -e "CREATE TABLE data (agency varchar(255) not null, positive integer(4), positive_pct real, negative integer(4), negative_pct real);\n.mode csv\n.import 'layers/$question.csv' data" | sqlite3 layers/$question.sqlite
